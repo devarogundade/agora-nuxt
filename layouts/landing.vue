a8f29e01a50921a24c55d9ac64e49c098798f067
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-09 13:32:46 +0100

    commit

---
+++ layouts/landing.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="root">
+    <TopBar />
+  </div>
+</template>
+
+<style scoped>
+.root {
+  background: rgb(7, 7, 100);
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    layout: 'landing'
+}
+</script>
+
+<style scoped>
+
+</style>
