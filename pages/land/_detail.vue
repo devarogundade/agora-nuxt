2ff7ec52ca61bdebf74aab44cc290f8b09737287
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-10 23:00:06 +0100

    commit

--- pages/land/_detail.vue
+++ pages/land/_detail.vue
@@ -32,6 +32,26 @@
                         </div>
                     </div>
                 </div>
+
+                <ul>
+                    <li>
+                        <i class="fi fi-rr-time-past"></i>
+                        <p>5 leases</p>
+                        <div class="progress"></div>
+                    </li>
+
+                    <li>
+                        <i class="fi fi-rr-star"></i>
+                        <p>4 of 5 star</p>
+                        <div class="progress"></div>
+                    </li>
+
+                    <li>
+                        <i class="fi fi-rr-shield-check"></i>
+                        <p>Verified item</p>
+                        <div class="progress"></div>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>
@@ -149,6 +169,63 @@ section {
     justify-content: center;
 }
 
+.text ul {
+    margin-top: 30px;
+}
+
+.text li {
+    display: grid;
+    grid-template-columns: 40px auto;
+    height: 30px;
+    background: #3f5e66;
+    margin-bottom: 5px;
+    border-radius: 10px;
+    position: relative;
+    overflow: hidden;
+    width: 250px;
+    max-width: 100%;
+}
+
+.text .progress {
+    position: absolute;
+    height: 100%;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+.text li:first-child .progress {
+    width: 100%;
+    background: #4577ff;
+}
+
+.text li:nth-child(2) .progress {
+    width: 80%;
+    background: #fac20a;
+}
+
+.text li:last-child .progress {
+    width: 70%;
+    background: #99b4ff;
+}
+
+.text li i {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+}
+
+.text li p {
+    z-index: 1;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 15px;
+    font-weight: 400;
+}
+
 @media screen and (max-width: 900px) {
     .grid {
         grid-template-columns: 100%;
@@ -156,11 +233,11 @@ section {
     }
 
     .f-back {
-      margin-top: 150px;
+        margin-top: 150px;
     }
 
     .grid {
-      margin-top: -20px;
+        margin-top: -20px;
     }
 }
 </style>
