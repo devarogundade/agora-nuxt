b9d6b156151362a22d66c54aef86dab50cd55a5e
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-10 22:37:04 +0100

    commit

--- pages/land/_detail.vue
+++ pages/land/_detail.vue
@@ -10,6 +10,31 @@
             </a>
         </div>
     </section>
+
+    <section>
+        <div class="app-min-width grid">
+            <div class="image"></div>
+            <div class="text">
+                <div class="name">
+                    <h3>CAT 2005</h3>
+                    <p>Machinery</p>
+                </div>
+
+                <div class="price">
+                    <div class="stock">
+                        Availability <span>In stock</span>
+                    </div>
+                    <div class="amount">
+                        <p class="fixed">Rate per month</p>
+                        <h3>$50.00</h3>
+                        <div class="btn">
+                            Request
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 </template>
 
@@ -24,6 +49,7 @@ section {
     display: flex;
     justify-content: center;
     margin-top: 50px;
+    color: #ffffff;
 }
 
 .back {
@@ -34,7 +60,6 @@ section {
     background: #003543;
     overflow: hidden;
     width: fit-content;
-    color: #ffffff;
 }
 
 .back i {
@@ -51,6 +76,76 @@ section {
     align-items: center;
     padding-right: 10px;
     font-size: 14px;
+    color: #ffffff;
     font-weight: 400;
+}
+
+.grid {
+    display: grid;
+    grid-template-columns: 400px auto;
+    column-gap: 40px;
+}
+
+.name h3 {
+  font-size: 36px;
+  line-height: 46px;
+  font-weight: 500;
+}
+
+.name p {
+  font-size: 14px;
+  font-weight: 400;
+  opacity: 0.6;
+}
+
+.image {
+    background: #003543;
+    border-radius: 10px;
+    height: 450px;
+}
+
+.price {
+    margin-top: 20px;
+    border: 1px #003543 solid;
+    border-radius: 10px;
+    overflow: hidden;
+}
+
+.stock {
+    height: 50px;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px #003543 solid;
+    font-size: 14px;
+}
+
+.stock span {
+    padding: 2px 8px;
+    border-radius: 10px;
+    background: #99f1cd;
+    margin-left: 10px;
+    color: #00181e;
+}
+
+.amount {
+    padding: 20px;
+    background: #003543;
+}
+
+.btn {
+    height: 50px;
+    background: #00dc82;
+    border-radius: 12px;
+    width: 200px;
+    color: white;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+    font-size: 17px;
+    font-weight: 500;
+    margin-top: 20px;
+    justify-content: center;
 }
 </style>
