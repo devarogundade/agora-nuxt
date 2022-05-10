a81e041d9cbeeb40ddb0e4d6094753e1666b3d5f
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-10 19:43:45 +0100

    commit

--- components/LeaseAd.vue
+++ components/LeaseAd.vue
@@ -1,8 +1,11 @@
 <template>
 <section>
-    <div class="app-width">
+    <div class="app-min-width">
         <div class="ad">
-
+            <p>Do you have a land? Why not lease to farming and start earning monthly.</p>
+            <a href="">
+                <div class="button">Learn more</div>
+            </a>
         </div>
     </div>
 </section>
@@ -19,9 +22,53 @@ section {
 .ad {
     background: #99aeb4;
     border-radius: 20px;
-    height: 200px;
+    min-height: 150px;
     width: 100%;
     user-select: none;
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 24px;
+    background-image: url('/images/leaf.png');
+    background-position: 200px;
+    background-attachment: fixed;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+.ad p {
+    font-size: 24px;
+    color: #161704;
+    font-weight: 500;
+    margin-right: 20px;
+}
+
+.ad .button {
+    padding: 10px 25px;
+    border-radius: 10px;
+    background: #002028;
+    color: white;
+    font-weight: 600;
+    font-size: 15px;
+    width: max-content;
+}
+
+@media screen and (max-width: 800px) {
+    .ad {
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 15px;
+    }
+
+    .ad p {
+        margin-bottom: 10px;
+        font-size: 20px;
+    }
+
+    .ad .button {
+        padding: 8px 20px;
+    }
+
 }
 </style>
