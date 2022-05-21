a377af8b70a968a3df3ffeca82b664f8545d7d55
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-21 20:28:24 +0100

    commit

--- components/LeaseTimeline.vue
+++ components/LeaseTimeline.vue
@@ -13,7 +13,7 @@
 
         <div class="steps">
             <p>Offer accepted</p>
-            <p>{{ offer.user.name }} has successfully pass verification and screening process</p>
+            <p>{{ offer.user.name }} has successfully pass verification process</p>
             <p>{{ offer.user.name }} now has access to utilize {{ offer.asset.type }} located at {{ offer.asset.location }}</p>
             <p>This offer will end at {{ offer.expires_at }}</p>
         </div>
