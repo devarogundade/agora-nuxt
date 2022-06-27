79299abce871b53457882d9c069790891e4095d2
Arogundade Ibrahim <devarogundade@gmail.com>
2022-06-27 18:44:40 +0100

    update minor changes on bottom nav

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -16,7 +16,7 @@
                 </div>
 
                 <div class="child">
-                    <h3>Others</h3>
+                    <h3>Features</h3>
 
                     <ul>
                         <li><a href="/farm-guide">Farm Guide</a></li>
